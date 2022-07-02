e3c3f36429099b9318a6036838da4df57af261fb
jkim552 <claymorejh4@gmail.com>
2022-07-01 22:11:00 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? 'red' : 'black'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Task Management',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
